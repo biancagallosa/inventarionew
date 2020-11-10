5e2f674f1f9baee81cf04c63d6c35b507df10cb0
biancagallosa <62562095+biancagallosa@users.noreply.github.com>
2020-11-10 03:59:57 +0100

    Add files via upload

---
+++ Persona12.java
@@ -0,0 +1,28 @@
+package Ejercicio;
+
+public class Persona12 {
+		
+
+	
+	    String Nombre ="Bianca";
+	    String Apellido = "Gallosa";
+		String numero_de_cedula = "28408259";
+		String fecha_nacimiento = "26/12/2001";
+		
+		public void Datos() {
+			
+			System.out.println("Su nombre es " + Nombre + Apellido);
+			System.out.println("Cedula de identidad: " +numero_de_cedula);
+			System.out.println(" y su Fecha de nacimiento es el "+fecha_nacimiento);
+			
+			
+		
+	}
+				
+  
+}
+		
+
+	
+
+
---
+++ Principal.java
@@ -0,0 +1,13 @@
+package Ejercicio;
+
+public class Principal {
+
+	public static void main(String[] args) {
+	
+		
+			Persona12 mensajero =new Persona12();
+			
+			mensajero.Datos();
+	}
+
+}
