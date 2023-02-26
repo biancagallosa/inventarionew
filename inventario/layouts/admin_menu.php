c99b387d80643351d220963c5f2e0380263f1f2e
biancagallosa <62562095+biancagallosa@users.noreply.github.com>
2023-02-25 23:30:22 -0400

    Add files via upload

---
+++ inventario/layouts/admin_menu.php
@@ -0,0 +1,61 @@
+<ul>
+  <li>
+    <a href="admin.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Panel de control</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-user"></i>
+      <span>Accesos</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="group.php">Administrar grupos</a> </li>
+      <li><a href="users.php">Administrar usuarios</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="categorie.php" >
+      <i class="glyphicon glyphicon-indent-left"></i>
+      <span>Categorías</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-large"></i>
+      <span>Productos</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="product.php">Administrar productos</a> </li>
+       <li><a href="add_product.php">Agregar productos</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="media.php" >
+      <i class="glyphicon glyphicon-picture"></i>
+      <span>Media</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-list"></i>
+       <span>Distribución</span>
+      </a>
+      <ul class="nav submenu">
+         <li><a href="sales.php">Administrar Distribución</a> </li>
+         <li><a href="add_sale.php">Agregar Distribución</a> </li>
+     </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-signal"></i>
+       <span>Reporte de Distribución</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="sales_report.php">Distribucion por fecha </a></li>
+        <li><a href="monthly_sales.php">Distribucion mensuales</a></li>
+        <li><a href="daily_sales.php">Distribucion diarias</a> </li>
+      </ul>
+  </li>
+</ul>
---
+++ inventario/layouts/user_menu.php
@@ -0,0 +1,29 @@
+<ul>
+  <li>
+    <a href="home.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Panel de control</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-list"></i>
+       <span>Distribución</span>
+      </a>
+      <ul class="nav submenu">
+         <li><a href="sales.php">Administrar Distribución</a> </li>
+         <li><a href="add_sale.php">Agregar Distribución</a> </li>
+     </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-signal"></i>
+       <span>Reporte de distribución</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="sales_report.php">Distribución por fecha </a></li>
+        <li><a href="monthly_sales.php">Distribución mensuales</a></li>
+        <li><a href="daily_sales.php">Distribución diarias</a> </li>
+      </ul>
+  </li>
+</ul>
